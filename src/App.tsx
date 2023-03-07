9afe845ddfcceb3dddbb6910dffdbd3709d2c234
Benjamin Prentice <benjaminprentice@Benjamins-MBP.home>
2023-03-07 08:44:53 -0600

    style nav links

---
+++ src/App.tsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import logo from './logo.svg';
+import MyComponent from './components/MyComponent/MyComponent';
+import './App.css';
+import Navbar from './components/Navbar/Navbar';
+import RouteLinks from './components/RouteLinks/RouteLinks';
+import {BrowserRouter} from 'react-router-dom';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+const dataList = ['Stellar', 'Coffee'];
+
+function App() {
+  return (
+    <div className="App">
+      <BrowserRouter>
+        <Navbar />
+        <RouteLinks/>
+      </BrowserRouter>
+      
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Navbar/Navbar.tsx
@@ -0,0 +1,25 @@
+import React, { FC } from 'react';
+import { NavLink } from 'react-router-dom';
+
+export default function Navbar() {
+  return (
+  <nav className='navbar'>
+    <div className='w-100 justify-content-center'>
+    <NavLink to="/" className='navbar-brand rounded'>
+      Home
+    </NavLink>
+    <NavLink to="/opportunities" className='navbar-brand rounded'>
+      Opportunities
+    </NavLink>
+    <NavLink to="/foodtrucks" className='navbar-brand rounded'>
+      Food Trucks
+    </NavLink>
+    <NavLink to="/calculator" className='navbar-brand rounded'>
+      Calculator
+    </NavLink>
+    <NavLink to="/permits" className='navbar-brand rounded'>
+      Permits
+    </NavLink>
+    </div>
+  </nav>)
+}
